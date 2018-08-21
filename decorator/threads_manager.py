e4b7e8f8dc0e747b5c8a0228bdd2b92d56e33615
BaronMing <baron.ming@qq.com>
2018-08-21 20:18:07 +0800

    更新
    最后决定，不再支持省略参数

--- decorator/threads_manager.py
+++ decorator/threads_manager.py
@@ -1,88 +1,41 @@
 from threading import Thread
 
-"""
-# 使用方法：
-- 1.创建线程管理对象
-	```
-	threads = ThreadsManager()
-	```
-- 2.使用线程管理对象装饰要作为线程运行时的函数
-	```
-	@threads()
-	def func1():
-		pass
-		
-	@threads()
-	def func2():
-		pass
-	```
-- 3.线程启动
-	- 第一种方法：
-		```
-		func1()
-		func2()
-		threads.start()
-		```
-		启动threads内的所有线程
-	
-	- 第二种方法:
-		```
-		threads.start = {
-			func1(),
-			func2()
-		}
-		```
-		启动threads内的所有线程
-		
-	- 第三种方法:
-		```
-		func1()
-		func2()
-		```
-		运行函数即等于启动线程
 
-"""
+class ThreadManager:
+    def __init__(self, immediately=True):
+        self.immediately = immediately
+        self.threads = set()
 
+    def __call__(self, name=None, daemon=None):
+        def fn_wrapper(fn):
+            def args_wrapper(*args, **kwargs):
+                new_thread = Thread(target=fn, name=None, args=args, kwargs=kwargs, daemon=None)
+                if self.immediately:
+                    new_thread.start()
+                else:
+                    self.threads.add(new_thread)
 
-class ThreadsManager:
-	def __init__(self, immediately=True):
-		self.threads = set()
-		self.immediately = immediately
-	
-	def __call__(self, name=None, daemon=None):
-		def fn_wrapper(fn):
-			def args_wrapper(*args, **kwargs):
-				thread = Thread(target=fn, name=name, args=args, kwargs=kwargs, daemon=daemon)
-				
-				if self.immediately:
-					thread.start()
-				else:
-					self.threads.add(thread)
-				
-				return thread
-			
-			return args_wrapper
-		
-		return fn_wrapper
-	
-	def __start(self):
-		return [thread.start for thread in self.threads]
-	
-	@property
-	def start(self):
-		if self.threads:
-			def wrapper():
-				self.__start()
-			
-			return wrapper
-		else:
-			raise Exception('必须先调用进程函数')
-	
-	@start.setter
-	def start(self, _set):
-		if not _set:
-			raise TypeError('必须传入有效参数')
-		elif _set == self.threads:
-			self.__start()
-		else:
-			raise TypeError('类型为%s，要求参数类型为"集合"' % type(_set))
+                return new_thread
+
+            return args_wrapper
+
+        return fn_wrapper
+
+    def __start(self):
+        [thread.start() for thread in self.threads]
+
+    @property
+    def start(self):
+        if self.threads:
+            return lambda: self.__start()
+        else:
+            raise Exception('必须先调用进程函数')
+
+    @start.setter
+    def start(self, _set):
+        if not _set:
+            raise TypeError('集合不能为空，必须赋有效值')
+        elif _set.issubset(self.threads):
+            self.__start()
+        else:
+            raise TypeError('类型为%s，要求参数类型为"集合"' % type(_set))
