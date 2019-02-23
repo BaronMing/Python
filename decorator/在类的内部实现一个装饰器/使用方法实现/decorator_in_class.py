b07e1111d54710efc23bd70ae5b0b2cd032b8938
BaronMing <baron.ming@qq.com>
2019-02-23 14:13:00 +0800

    打卡
    Python 面向对象对保护做的并不好，变量私有本身也不是 Python 设计者的意图，所以...干脆去掉吧

--- decorator/在类的内部实现一个装饰器/使用方法实现/decorator_in_class.py
+++ decorator/在类的内部实现一个装饰器/使用方法实现/decorator_in_class.py
@@ -1,16 +1,14 @@
 class Example:
-    def __decorator(*dec_args, **dec_kwargs):
+    def decorator(*dec_args, **dec_kwargs):
         print(*dec_args, *dec_kwargs)
 
         def fn_wrapper(fn):
             def args_wrapper(self, *fn_args, **fn_kwargs):
                 return fn(self, *fn_args, **fn_kwargs)
-
             return args_wrapper
-
         return fn_wrapper
 
-    @__decorator()
+    @decorator()
     def run(self):
         print("running")
 
